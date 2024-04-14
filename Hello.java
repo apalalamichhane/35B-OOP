643c15e2a5265feb6e27d6f7b98386198a127600
apalalamichhane <apala13579@gmail.com>
2024-04-14 14:16:00 +0545

    Week 1 Task 1

--- Hello.java
+++ Hello.java
@@ -3,5 +3,9 @@ package OOP.Task;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hi");
+        System.out.println("Hello");
+
+        //This is a comment
+        /**This is a javadoc comment */
     }
 }
